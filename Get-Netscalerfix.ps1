ac8f2c76a2878a9ffa6b5992dd78d54684374a85
jakobo82 <jacobo.carballada@gmail.com>
2020-01-15 18:16:47 +0100

    update nssh

--- Get-Netscalerfix.ps1
+++ Get-Netscalerfix.ps1
@@ -1,4 +1,4 @@
-﻿Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -ErrorAction SilentlyContinue 
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -ErrorAction SilentlyContinue 
 install-module posh-ssh
 
 
@@ -20,6 +20,8 @@ if (Test-Connection $nsip) {
 $nscred = (Get-Credential -UserName nsroot -Message "netscaler pass")
 $ssh = New-SSHSession -ComputerName $nsip -Credential $nscred  -Port 22 -AcceptKey
 $newssh = New-SSHShellStream -SessionId $ssh.SessionId
+
+if ($newssh) {
 $newssh.WriteLine("shell")
 $newssh.WriteLine("ls /var/vpn/bookmark/*.xml")
 $newssh.WriteLine("cat /var/log/httpaccess.log | grep vpns | grep xml")
@@ -30,9 +32,10 @@ $newssh.WriteLine("cat /var/log/bash.log | grep nobody")
 $tempfile = "$($nsip)_$($env:USERDNSDOMAIN).log" 
 1..650 | foreach -process { $newssh.Readline($_) | out-file -Append $tempfile }
 Remove-SSHSession -Index $ssh.SessionId
+} else {throw "no possible connection ssh to the netscaler with $($nsip)" }
+
+
 } else { throw "no reacheable nsip address" }
 
 
 }
-
-
